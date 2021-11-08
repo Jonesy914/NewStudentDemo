a61a5fdf0f85d013426bd7d49e09d367776fa7d2
Jonesy914 <92300463+Jonesy914@users.noreply.github.com>
2021-11-08 10:22:05 +0000

    Created new Change Age button to Form1.

--- NewStudentDemo/Form1.Designer.cs
+++ NewStudentDemo/Form1.Designer.cs
@@ -36,60 +36,55 @@ namespace NewStudentDemo
             this.label3 = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnChangeAge = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(156, 103);
-            this.txtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtName.Location = new System.Drawing.Point(104, 67);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(181, 26);
+            this.txtName.Size = new System.Drawing.Size(122, 20);
             this.txtName.TabIndex = 0;
             // 
             // txtAge
             // 
-            this.txtAge.Location = new System.Drawing.Point(156, 163);
-            this.txtAge.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtAge.Location = new System.Drawing.Point(104, 106);
             this.txtAge.Name = "txtAge";
-            this.txtAge.Size = new System.Drawing.Size(181, 26);
+            this.txtAge.Size = new System.Drawing.Size(122, 20);
             this.txtAge.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(63, 108);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(42, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Name:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(63, 168);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(42, 109);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Age:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(152, 58);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(101, 38);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(112, 20);
+            this.label3.Size = new System.Drawing.Size(75, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Add a Student";
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(156, 237);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAdd.Location = new System.Drawing.Point(104, 154);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(112, 35);
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
             this.btnAdd.TabIndex = 5;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -97,19 +92,32 @@ namespace NewStudentDemo
             // 
             // btnDisplay
             // 
-            this.btnDisplay.Location = new System.Drawing.Point(156, 310);
+            this.btnDisplay.Location = new System.Drawing.Point(104, 201);
+            this.btnDisplay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDisplay.Name = "btnDisplay";
-            this.btnDisplay.Size = new System.Drawing.Size(112, 35);
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
             this.btnDisplay.TabIndex = 6;
             this.btnDisplay.Text = "Display";
             this.btnDisplay.UseVisualStyleBackColor = true;
             this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
             // 
+            // btnChangeAge
+            // 
+            this.btnChangeAge.Location = new System.Drawing.Point(104, 245);
+            this.btnChangeAge.Margin = new System.Windows.Forms.Padding(2);
+            this.btnChangeAge.Name = "btnChangeAge";
+            this.btnChangeAge.Size = new System.Drawing.Size(75, 23);
+            this.btnChangeAge.TabIndex = 7;
+            this.btnChangeAge.Text = "Change Age";
+            this.btnChangeAge.UseVisualStyleBackColor = true;
+            this.btnChangeAge.Click += new System.EventHandler(this.btnChangeAge_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChangeAge);
             this.Controls.Add(this.btnDisplay);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.label3);
@@ -117,7 +125,6 @@ namespace NewStudentDemo
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtAge);
             this.Controls.Add(this.txtName);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -134,6 +141,7 @@ namespace NewStudentDemo
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnChangeAge;
     }
 }
 
